2b44cfdcbf8a19554dcee5b2225020d60c5a83d3
Konstantinos Artopoulos <artopouloskon@gmail.com>
2024-12-11 12:58:22 +0200

    feat(nvim): limit treesitter-context lines to 2

--- .config/nvim/lua/custom/plugins/treesitter-context.lua
+++ .config/nvim/lua/custom/plugins/treesitter-context.lua
@@ -5,7 +5,7 @@ return {
 		require("treesitter-context").setup({
 			enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
 			throttle = true, -- Throttles plugin updates (may improve performance)
-			max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit.
+			max_lines = 2, -- How many lines the window should span. Values <= 0 mean no limit.
 			patterns = {
 				-- Match patterns for TS nodes. These get wrapped to match at word boundaries.
 				-- For all filetypes
