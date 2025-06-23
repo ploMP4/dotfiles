2777248bb080a68a97c1239388d9f61779e1e6b7
Konstantinos Artopoulos <artopouloskon@gmail.com>
2025-06-23 16:57:54 +0300

    fix(nvim): use new vim.lsp.config to configure lsp servers instead of mason

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -176,18 +176,12 @@ return { -- LSP Configuration & Plugins
 		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-		require("mason-lspconfig").setup({
-			handlers = {
-				function(server_name)
-					local server = servers[server_name] or {}
-					-- This handles overriding only values explicitly passed
-					-- by the server configuration above. Useful when disabling
-					-- certain features of an LSP (for example, turning off formatting for tsserver)
-					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-					require("lspconfig")[server_name].setup(server)
-				end,
-			},
-		})
-		require("lspconfig").gleam.setup({})
+		-- Installed LSPs are configured and enabled automatically with mason-lspconfig
+		-- The loop below is for overriding the default configuration of LSPs with the ones in the servers table
+		for server, config in pairs(servers) do
+			vim.lsp.config(server, config)
+		end
+
+		require("mason-lspconfig").setup({})
 	end,
 }
